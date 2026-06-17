7887e90c12295e9a9fe98eba709a2bfbce88c109
Zac <128709919+blueyak48@users.noreply.github.com>
2026-06-17 15:41:34 -0500

    pwa improvements

---
+++ resize.ps1
@@ -0,0 +1,17 @@
+Add-Type -AssemblyName System.Drawing
+$img = [System.Drawing.Image]::FromFile("a:\RailSummaryPWA\icon.jpg")
+
+function Save-ResizedImage {
+    param([int]$size, [string]$path)
+    $bmp = New-Object System.Drawing.Bitmap($size, $size)
+    $g = [System.Drawing.Graphics]::FromImage($bmp)
+    $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+    $g.DrawImage($img, 0, 0, $size, $size)
+    $g.Dispose()
+    $bmp.Save($path, [System.Drawing.Imaging.ImageFormat]::Png)
+    $bmp.Dispose()
+}
+
+Save-ResizedImage 192 "a:\RailSummaryPWA\icon-192.png"
+Save-ResizedImage 512 "a:\RailSummaryPWA\icon-512.png"
+$img.Dispose()
--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'rail-summary-v1';
+const CACHE_NAME = 'rail-summary-v2';
 const ASSETS = [
     './',
     './index.html',
     './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
     'https://cdn.tailwindcss.com',
     'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
